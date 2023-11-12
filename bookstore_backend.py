01165b9df9f3c40f2625369561009f23abdf7ec3
tejas244 <117664421+Tejkh1@users.noreply.github.com>
2023-11-12 06:31:35 +0530

    Update bookstore_backend.py
    added function to insert and view book

--- bookstore_backend.py
+++ bookstore_backend.py
@@ -15,3 +15,20 @@ def connect():
     """)
     con.commit()  # commit the changes to the database
     con.close()  # closes the database
+
+def insertbook(title, author, year, isbn):
+    con = sqlite3.connect("bookstore.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO books (title, author, year, isbn) VALUES (?, ?, ?, ?)",
+                (title, author, year, isbn))
+    con.commit()
+    con.close()
+
+def viewstore():
+    conn = sqlite3.connect("bookstore.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM bookstore")
+    rows = cur.fetchall()
+    conn.close
+    return rows
+
